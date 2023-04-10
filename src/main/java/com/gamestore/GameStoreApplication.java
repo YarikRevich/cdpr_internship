01f5a5ce44863a273acde931453d6165d5ac4c2c
YarikRevich <yariksvitlitskiy2@gmail.com>
2023-04-10 22:17:10 +0200

    fix: fixed swagger api

--- src/main/java/com/gamestore/GameStoreApplication.java
+++ src/main/java/com/gamestore/GameStoreApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.gamestore.GameStoreApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
 public class GameStoreApplication {
 	public static void main(String[] args) {
