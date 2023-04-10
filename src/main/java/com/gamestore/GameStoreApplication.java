e9b631a742280f49e6f5473f30de05d8ecddf460
YarikRevich <yariksvitlitskiy2@gmail.com>
2023-04-10 22:22:11 +0200

    fix: fixed bugs

--- src/main/java/com/gamestore/GameStoreApplication.java
+++ src/main/java/com/gamestore/GameStoreApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.gamestore.GameStoreApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Game store API", version = "1.0"))
 public class GameStoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GameStoreApplication.class, args);
